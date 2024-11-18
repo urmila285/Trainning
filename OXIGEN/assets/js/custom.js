dcf6fb0c054590357c718a3a5fc4474a3860b501
urmila285 <urmilagithub@gmail.com>
2024-11-18 21:24:16 +0530

    oxigen

---
+++ OXIGEN/assets/js/custom.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  /** sidenav **/
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
