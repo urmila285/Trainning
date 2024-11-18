144d67211b5eb9b503ed0562598acc656d02896e
urmila285 <urmilagithub@gmail.com>
2024-11-18 21:18:51 +0530

    Add files via upload

---
+++ Gorjana/assets/js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
